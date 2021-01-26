975c1d8cb48a77d6156187c944e6983157ea6cf9
trifon shterev <tshterev@abv.bg>
2021-01-26 13:08:55 +0200

    code must be fixed

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import CardList from './CardList.js';
+import SearchBox from './SearchBox'
+import {robots} from './robots';
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state= {
+            robots: robots,
+            searcfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searcfield: event.target.value})     
+       
+    }
+
+    render(){
+        const filteredRobots= this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+
+        return (
+            <div className= 'tc'>
+                <h1>Robofriends</h1>
+                <SearchBox searchChange= {this.onSearchChange} />
+                <CardList  robots={filteredRobots}/>
+            </div>
+                
+            )
+    }
+
+
+}
+export default App;
